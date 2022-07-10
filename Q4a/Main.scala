7fbfa936e43452555b336d979f2ee5e30622905c
Hasindu Sudeepana <108721333+HasinduSudeepana@users.noreply.github.com>
2022-07-10 19:55:00 +0530

    T added an answer to the question 4.a

--- Q4a/Main.scala
+++ Q4a/Main.scala
@@ -2,19 +2,19 @@ object Main {
   def main(args: Array[String]): Unit = {
     println("Salary:"+Pay_salary(40,30))
   }
-  def normal_salary(x:Int):Int={
+  def normal_salary(x:Int):Int={   //calculate nomal salary using normal working hours
     return 250*x
   }
-  def Ot_salary(y:Int):Int={
+  def Ot_salary(y:Int):Int={     //calculate the ot salary using ot working hours
     return 85*y
   }
-  def Total_salary(x:Int,y:Int):Int={
+  def Total_salary(x:Int,y:Int):Int={  //calculate total salary using normal salary + ot salary
     return normal_salary(x)+Ot_salary(y)
   }
-  def Tax(x:Int,y:Int):Double={
+  def Tax(x:Int,y:Int):Double={     //calculate the tax value using total salary*12%
     return Total_salary(x,y)*(12.0/100.0)
   }
-  def Pay_salary(x:Int,y:Int):Double={
+  def Pay_salary(x:Int,y:Int):Double={   // calculate net value using total salary -tax value
     return Total_salary(x,y)-Tax(x,y)
   }
 }
